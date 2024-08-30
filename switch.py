9600b693beb08af9807f4bbd24e185fa979b3e20
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-08-30 09:13:19 +0530

    Update switch.py

--- switch.py
+++ switch.py
@@ -5,8 +5,15 @@ import hashlib
 import socket
 from time import sleep
 import time
+#import threading
+
 #app=Flask(__name__)
 key=input("KEY")
+#challenge=requests.get("https://www.uuidgenerator.net/api/version1")
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+#sock.bind(('192.168.29.255',5005))
+
 #@app.route('/', methods=['GET'])
 def get_key():
 #    if request.method == 'GET':
@@ -24,27 +31,31 @@ def get_key():
         print(f'sending on {ip}')
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.bind(('192.168.29.255',5005))
-        sock.settimeout(10)
-        while True:
-            print(msg,len(msg))
-            sock.sendto(msg, ("192.168.29.255", 5005))
-            time.sleep(1)
+        sock.bind(('',5005))
+        #sock.settimeout(10)
+        print(msg,len(msg))
+        sock.sendto(msg, ("192.168.29.255", 5005))
+#        time.sleep(10)
         sock.close()
-
+#def receive():
         #type is strings, challenge hashed with K2=CK2
-        challenge=challenge.content
-        signed=hmac_sha256(key,challenge)
+        #msg2=challenge.content
+        signed=(hmac_sha256(key,msg)).encode()
         
         #receiving CK1
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPRONTO_UDP)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.bind("192.168.29.255", 5005)
+        sock.bind(("", 6005))
         while True:
-            ck1, addr = sock.recvfrom(1024)
-            if(ck1==challenge):
-                print("verified")
-        
+            try:
+                ck1, addr = sock.recvfrom(1024)
+                if(ck1==signed):
+                    print(f'The received chslleneg is : {ck1} and received challenge is {signed}')
+                    print("verified")
+                else:
+                    print(f'signed challenge from client is {ck1} and signed challenge in server is {signed}')
+            except:
+                pass
 
 
     #return render_template("admin.html")
@@ -52,11 +63,16 @@ def get_key():
 def hmac_sha256(key, message):
   return hmac.new(
     key.encode("utf-8"),
-    message.encode("utf-8"),
+    message,
     hashlib.sha256
   ).hexdigest()
  
-
+#t1=threading.Thread(target=get_key)
+#t2=threading.Thread(target=receive)
+#t1.start()
+#t2.start()
 get_key()
+
+
 #if __name__ == '__main__':
 #    app.run()
