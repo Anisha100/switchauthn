78393fda0652d38bb7c5938d9f131215c486ecba
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-08-30 09:10:33 +0530

    Update client.py

--- client.py
+++ client.py
@@ -9,12 +9,21 @@ def get_ch():
 	#get challenge
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	sock.bind(("192.168.29.255", 5005))
-	sock.settimeout(10)
+	sock.bind(("", 5005))
+	#sock.connect(("192.168.29.248",))
+	#sock.settimeout(10)
 
 	while True:
-		challenge, addr = sock.recvfrom(1024)
+	#
+		try:
+			challenge, addr = sock.recvfrom(1024)
+			print(challenge)
+			break
+		except:
+			pass
 	sock.close()
+	time.sleep(2)
+
 	#type is strings, generate CK1
 	#challenge=challenge.content
 	signed=hmac_sha256(key,challenge)
@@ -23,22 +32,22 @@ def get_ch():
 	#broadcast CK1
 	#interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
 	#allips = [ip[-1][0] for ip in interfaces]
-	msq=signed
+	msg=signed
 	print(f'sending on {msg}')
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	sock.bind('192.168.29.255',5005)
-	sock.settimeout(10)
+	sock.bind(('',6005))
+	#sock.settimeout(10)
 	while True:
-			print(msg,len(msg))
-			sock.sendto(msg, ("192.168.29.255", 5005))
+			print(msg.encode(),len(msg))
+			sock.sendto(msg.encode(), ("192.168.29.255", 6005))
 			time.sleep(1)
 	sock.close()
 		
 def hmac_sha256(key, message):
   return hmac.new(
     key.encode("utf-8"), 
-    message.encode("utf-8"), 
+    message, 
     hashlib.sha256
   ).hexdigest()
   
