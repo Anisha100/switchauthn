53eb43badfeaa575eefca73f3c1e718ebe08d4f6
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-09-09 10:35:58 +0530

    Update cloud.py

--- chatbot/TCP/cloud.py
+++ chatbot/TCP/cloud.py
@@ -1,28 +1,32 @@
 import socket
 
 s1=socket.socket()
+s1.bind(('',4004))
+s1.listen(5)
 s2=socket.socket()
+s2.bind(('',5005))
+s2.listen(5)
+c1,addr1=s1.accept()
+c2,addr2=s2.accept()
+
 port1=4004
 port2=5005
 ip='20.198.19.177'
 
-def receive():
-       while True:  
-             s1.connect((ip,port1))
-             msg=s1.recv(1024).decode())
-             print(msg)
-             
-                    
-def sending():
-	while True:
-    s2.bind('',port2)
-    c,addr=s2.accept()
-		msg=input("message :  ")
-		c.send(msg)
+def A():
+       while True:
 		
+		x=c1.recv(1024))
+		c2.send(x)		    
+                    
+def B():
+	while True:
+		
+		x=c2.recv(1024))
+		c1.send(x)	
 		             
-t1=threading.Thread(targe=sending)
-t2=threading.Thread(target=receive)
+t1=threading.Thread(target=A)
+t2=threading.Thread(target=B)
 t1.start()
 t2.start()
-get_key()    
+   
